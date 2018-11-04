d0a5e63d3b56fba0bc8a0cdd6948eb20482fa3cd
Abhijeet Muneshwar <openingknots@gmail.com>
2018-11-04 18:15:36 +0530

    Scala Tuple

--- src/main/scala/Tuples.scala
+++ src/main/scala/Tuples.scala
@@ -4,8 +4,13 @@
   * Date:    04/11/18,
   * Time:    5:00 PM
   */
+
 object Tuples {
   def main(args: Array[String]): Unit = {
-    //
+    var tupleMerge = (103, "Thomas", 10.25)
+    printf("%s ows us %.2f\n", tupleMerge._2, tupleMerge._3)
+
+    tupleMerge.productIterator.foreach { i => println(i) }
+    println(tupleMerge.toString())
   }
 }
