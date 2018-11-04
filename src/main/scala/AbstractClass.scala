9dba21d08ec5cf579bd5299e870df33af04f162c
Abhijeet Muneshwar <openingknots@gmail.com>
2018-11-04 22:35:33 +0530

    Scala Class and Abstract Class

--- src/main/scala/AbstractClass.scala
+++ src/main/scala/AbstractClass.scala
@@ -5,5 +5,22 @@
   * Time:    10:28 PM
   */
 object AbstractClass {
+  def main(args: Array[String]): Unit = {
+    val fang = new Wolf("Fang")
+    fang.moveSpeed = 36
+    println(fang.move)
+  }
+
+  abstract class Mammal(val name: String) {
+    var moveSpeed: Double
+
+    def move: String
+  }
+
+  class Wolf(name: String) extends Mammal(name) {
+    var moveSpeed = 35.9
+
+    def move = "The wolf %s runs %.2f km/hr".format(this.name, this.moveSpeed)
+  }
 
 }
